53afaef49eae16a1567c1c663f7bb207e89e5ddc
jcambray <jerome.cambray@gmail.com>
2015-05-18 14:33:27 +0200

    first commit

---
+++ src/serveur/Application.java
@@ -0,0 +1,13 @@
+package serveur;
+
+
+public class Application {
+	
+	
+	public static void main(String[] args) {
+		
+		HttpStaticServer s = new HttpStaticServer();
+		s.run();
+	}
+
+}
---
+++ src/serveur/HttpStaticServer.java
@@ -0,0 +1,97 @@
+package serveur;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+public class HttpStaticServer {
+	
+
+	private ServerSocket serverSocket;
+	private Socket clientSocket;
+	private Map<String, String> domainsMap;
+	
+	private final static int port = 8181;
+	
+	
+	public HttpStaticServer()
+	{
+		try {
+			serverSocket = new ServerSocket(port);
+			domainsMap = new HashMap<String, String>();
+			domainsMap.put("my.website.com", "C:\\www\\index");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+	
+	public void run()
+	{
+		
+		while(true)
+		{
+			Socket s;
+			List<String> socketData = new ArrayList<String>();
+			try {
+				s = serverSocket.accept();
+				System.out.println("connecte");
+				BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+				String inputLine;
+				while (!(inputLine = in.readLine()).equals(""))
+				{
+					socketData.add(inputLine);
+				    System.out.println(inputLine);    
+				}
+				in.close();
+				String getValue = "";
+				String hostValue = "";
+				parseData(socketData,getValue,hostValue);
+				String Path = domainsMap.get(hostValue);
+				if(!getValue.equals(""))
+				{
+					File f = new File(getValue);
+					 ObjectOutputStream oos = new ObjectOutputStream(s.getOutputStream());
+					 oos.writeObject(f);
+					 oos.flush();
+					 oos.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+		}
+			
+	}
+	
+	private void parseData(List<String> dataList,String getval,String hostVal)
+	{
+		for (String line : dataList) {
+			String upperLine = line.toUpperCase();
+			if(upperLine.startsWith("GET"))
+			{
+				getval = line.split(" ")[1];
+			}
+			if(upperLine.startsWith("HOST"))
+			{
+				hostVal = line.split(" ")[1]
+						.split(":")[0];
+			}
+		}
+	}
+	
+	
+}
+	
+	
+	
